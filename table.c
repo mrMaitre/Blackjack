24ba606f97f275d1977fc31c14feb24a8e57869f
mrMaitre <92534768+mrMaitre@users.noreply.github.com>
2022-04-30 02:02:46 +0200

    initialistaion de la table avec les joueurs

---
+++ table.c
@@ -0,0 +1,54 @@
+#include "table.h"
+
+TABLE* init_table(){
+    TABLE * table;
+    table = (TABLE *) malloc(sizeof(TABLE));
+    table->nb_joueurs=0;
+    table->tete=NULL;
+    return table;
+}
+
+void saisie_joueurs_dans_table(TABLE* table){
+    int nb,i;
+    /* initialisation du nombre de joueurs dans la table */
+    do {
+        printf("\nCombien y a t-il de joueurs sur la table (max %d) ? ",MAX_JOUEURS);
+        scanf("%d",&nb);
+    } while (nb<0 || nb>5);
+    table->nb_joueurs=nb;
+
+    /* initialisation des joueurs dans la table */
+    JOUEUR* j,*j_suiv;
+    printf("\n---------- Saisie des joueurs dans la table ----------\n");
+    j=saisie_joueur(1);
+    table->tete=j;
+    for (i=2;i<=nb;i++){
+        j_suiv=saisie_joueur(i);
+        j->suivant=j_suiv;
+        j=j_suiv;
+    }
+}
+
+int table_est_pleine(TABLE* t){
+    if(t->nb_joueurs==MAX_JOUEURS) return 1;
+    return 0;
+}
+
+int table_est_vide(TABLE* t){
+    if(t->nb_joueurs==0) return 1;
+    return 0;
+}
+
+void affiche_table(TABLE* t){
+    JOUEUR *j;
+    printf("\n---------- Affichage de la table ----------\n");
+    if (table_est_vide(t)==1) printf("La table est vide");
+    else{
+        j=t->tete;
+        while(j!=NULL){
+            affiche_joueur(j);
+            j=j->suivant;
+        }
+    }
+}
+
---
+++ table.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "joueur.h"
+
+#define MAX_JOUEURS 5
+
+typedef struct table{
+    int nb_joueurs;
+    JOUEUR * tete;
+} TABLE;
+
+TABLE* init_table();
+int table_est_pleine(TABLE* p);
+int table_est_vide(TABLE* p);
+void saisie_joueurs_dans_table(TABLE* table);
+void affiche_table(TABLE* p);
