4343e0814e99d6dc29b1041b6a9e09035f882aaa
Dorian Sartori <dorian.sartori@etu.univ-savoie.fr>
2022-05-11 10:35:29 +0200

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -20,7 +20,7 @@ typedef struct joueur{
 
 JOUEUR* saisie_joueur(int num);
 void affiche_joueur(JOUEUR* j);
-void tirer_carte_joueur_debut(JOUEUR *j);
+void tirer_carte_joueur_debut(PIOCHE *p,JOUEUR *j);
 void tirage_carte_croupier_mises(PIOCHE *p,JOUEUR *j);
 void liberer_joueur(JOUEUR *j);
 
