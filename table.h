95c4459739639319e69d13d17fec1c898f48cb88
Dorian Sartori <dorian.sartori@etu.univ-savoie.fr>
2022-05-11 11:18:28 +0200

    Update table.h

--- table.h
+++ table.h
@@ -18,13 +18,14 @@ typedef struct table{
 TABLE* init_table();
 int table_est_pleine(TABLE* p);
 int table_est_vide(TABLE* p);
-void saisie_joueurs_dans_table(TABLE* table);
+void saisie_joueurs_croupier_dans_table(TABLE* table);
 void affiche_table(TABLE* p);
 void demande_mises(TABLE* p);
 int comptage_score_croupier(CROUPIER *c);
 int comptage_score_joueurs(JOUEUR *j);
 int compter_score(CARTE* tab_cartes, int nb_cartes);
 void repartition_gains(TABLE *t);
+void tirage_debut_partie(PIOCHE *p,TABLE *t);
 void reste_sur_table(TABLE *t);
 
 #endif //TABLE_H_INCLUDED
